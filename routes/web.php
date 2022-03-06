d82a568b5007459c41efeb6dcf3050e8cbea1790
cincenfrank <fpetrangeli1@gmail.com>
2022-03-06 15:32:23 +0100

    added order routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('customers', 'CustomersController')->except('show')->middleware('auth');
+Route::resource('orders', 'OrdersController')->except('show')->middleware('auth');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
